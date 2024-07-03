99abee47009aa55f5188554805f3ddeef94ebe99
Bernardo <beefreguglia@gmail.com>
2024-07-03 17:10:25 -0300

    feature: init project

---
+++ src/server.js
@@ -0,0 +1,27 @@
+require("express-async-errors");
+
+const express = require("express");
+const AppError = require("./utils/AppError");
+const routes = require("./routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.use((error, _, response, _) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+  console.log(error);
+  return response.status(500).json({
+    status: "error",
+    "message": "Internal server error"
+  });
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
