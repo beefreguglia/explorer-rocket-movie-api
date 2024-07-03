2cc735e7814d071624143037885d99160284ef9d
Bernardo <beefreguglia@gmail.com>
2024-07-03 18:53:45 -0300

    feature: create user create

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,6 +4,26 @@ const knex = require("../database/knex");
 
 class UsersController {
   async create(request, response) {
+    const { 
+      name,
+      email,
+      password,
+    } = request.body;
+
+    const checkUserExists = await knex("users").where({ email }).first();
+
+    if(checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.")
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    await knex("users").insert({
+      name,
+      email,
+      password: hashedPassword
+    });
+
     return response.json();
   }
 }
--- src/database/knex/migrations/20240703202530_createUsers.js
+++ src/database/knex/migrations/20240703202530_createUsers.js
@@ -1,8 +1,8 @@
 exports.up = (knex) => knex.schema.createTable("users", (table) => {
   table.increments("id");
-  table.text("name");
-  table.text("email");
-  table.text("password");
+  table.text("name").notNullable();
+  table.text("email").notNullable();
+  table.text("password").notNullable();
   table.text("avatar_url");
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
