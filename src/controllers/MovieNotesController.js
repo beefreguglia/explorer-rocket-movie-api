1762b28368dfdb7c48ecbffb7639a70c90a58b56
Bernardo <beefreguglia@gmail.com>
2024-07-04 00:14:52 -0300

    feature: create movie notes index

--- src/controllers/MovieNotesController.js
+++ src/controllers/MovieNotesController.js
@@ -2,6 +2,43 @@ const AppError = require("../utils/AppError");
 const knex = require("../database/knex");
 
 class MovieNotesController {
+  async index(request, response) {
+    const { user_id, title, tags } = request.query;
+
+    let movieNotes;
+
+    if(tags) {
+      const filterTags = tags.split(",").map((tag) => tag.trim());
+
+      movieNotes = await knex("movie_tags")
+      .select([
+        "movie_notes.id",
+        "movie_notes.title",
+        "movie_notes.user_id",
+      ])
+      .where("movie_notes.user_id", user_id)
+      .whereLike("movie_notes.title", `%${title}%`)
+      .whereIn("name", filterTags)
+      .innerJoin("movie_notes", "movie_notes.id", "movie_tags.movie_note_id")
+      .orderBy("movie_notes.title");
+
+    } else {
+      movieNotes = await knex("movie_notes")
+        .where({ user_id })
+        .whereLike("title", `%${title}%`)
+        .orderBy("title");
+    }
+    
+    const userTags = await knex("movie_tags").where({ user_id });
+    const movieNotesWithTags = movieNotes.map((movieNote) => {
+      const movieNoteTags = userTags.filter((tag) => tag.movie_note_id === movieNote.id);
+      return {
+        ...movieNote,
+        tags: movieNoteTags
+      }
+    })
+    return response.json(movieNotesWithTags);
+  }
   async create(request, response) {
     const { 
       title,
--- src/routes/movie_notes.routes.js
+++ src/routes/movie_notes.routes.js
@@ -4,6 +4,7 @@ const MovieNotesController = require("../controllers/MovieNotesController");
 const movieNotesRoutes = Router();
 const movieNotesController = new MovieNotesController();
 
+movieNotesRoutes.get("/", movieNotesController.index);
 movieNotesRoutes.post("/:user_id", movieNotesController.create);
 
 module.exports = movieNotesRoutes;
